618921f41e437ef8a2a051743997fe713ab2d249
임지훈 <zhoon95@gmail.com>
2023-05-19 20:14:40 +0900

    FEAT:PAGINATION

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import TestPage from "../pages/TestPage";
 import PreviewPage from "../pages/PreviewPage";
 import SettingPage from "../pages/SettingPage";
 import HomePage from "../pages/HomePage";
 import NaviagationBar from "../components/Navigation/NavigationBar";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./transition.css";
+
+function TransitionRoutes() {
+  let location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={500}>
+        <Routes location={location}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/preview" element={<PreviewPage />} />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 export default function RootRoute() {
   return (
     <BrowserRouter>
       {/* <NaviagationBar /> */}
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/preview" element={<PreviewPage />} />
-        <Route path="/setting" element={<SettingPage />} />
-      </Routes>
+      <TransitionRoutes />
     </BrowserRouter>
   );
 }
