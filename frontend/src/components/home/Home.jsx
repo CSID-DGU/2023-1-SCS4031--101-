81837a4efd04450054a27274483f5621b45a0eaa
임지훈 <zhoon95@gmail.com>
2023-05-26 15:41:04 +0900

    FEAT:HOMEPAGE

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./Header";
 import BoardImg from "../../assets/HomeBoard.png";
+import ConvertImg from "../../assets/ConvertImg.png";
 
 const Home = () => {
   return (
@@ -9,6 +10,21 @@ const Home = () => {
       <Header />
       <div className="boardBx">
         <img src={BoardImg} />
+        <div className="textWrap">
+          <div className="textBx1">
+            <div>색각 이상자를 위한</div>
+            <div>비디오 변환 서비스입니다</div>
+          </div>
+          <div className="textBx2">
+            빠르게 실시간으로 비디오를 변환해 보세요!
+          </div>
+          <div className="textBx3">
+            비디오 변환하기
+            <div className="imgWrap">
+              <img src={ConvertImg} />
+            </div>
+          </div>
+        </div>
       </div>
     </Wrap>
   );
@@ -24,14 +40,65 @@ const Wrap = styled.div`
   font-family: "Jua", sans-serif;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
 
   .boardBx {
+    display: flex;
     width: 80vw;
+    position: relative;
 
     img {
+      position: relative;
       width: 100%;
+    }
+
+    .textWrap {
+      position: absolute;
+      top: 50%;
+      left: 20%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      width: 60%;
+
+      .textBx1 {
+        color: #ff14b1;
+        text-align: center;
+        font-size: 5.5rem; // 미디어 쿼리로 나중에 폰트 사이즈 변경하기
+      }
+
+      .textBx2 {
+        text-align: center;
+        color: #fff;
+        margin-top: 40px;
+        font-size: 1.5rem;
+      }
+
+      .textBx3 {
+        cursor: pointer;
+        text-align: center;
+        color: #fff;
+        border-radius: 7px;
+        background: rgba(255, 255, 255, 0.3);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px 20px;
+        white-space: nowrap;
+        font-size: 1.5rem;
+        margin-top: 40px;
+
+        .imgWrap {
+          width: 10%;
+          margin-left: 10px;
+
+          img {
+            width: 100%;
+          }
+        }
+      }
     }
   }
 `;
